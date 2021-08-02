ed1fee025295f9f8fdaf4013f0e671aef290d586
Neha Garg <68857416+Neha-520@users.noreply.github.com>
2021-08-03 01:15:56 +0530

    order backend

--- addtocart/server.js
+++ addtocart/server.js
@@ -43,7 +43,45 @@ app.delete("/api/products/:id", async(req,res)=>{
     res.send(deletedProduct);
 });
 
+//model for order
+const Order = mongoose.model("order",new mongoose.Schema({
+    _id:{
+        type: String,
+        default: shortId.generate
+    },
+    email: String,
+    name: String,
+    address: String,
+    total: Number,
+    //define products in order
+  cartItems: [{
+     _id:String,
+     title:String,
+     price: Number,
+     count:Number
+  }],//defined each elementt of array ie cartitem
+},{
+    timestamps: true, //when we create order two field will be added ie createdAt and updatedAt
+}
+)
+);
+
+app.post("/api/orders",async(req,res) =>{
+    if(!req.body.name ||
+       !req.body.email ||
+       !req.body.address ||
+       !req.body.total||
+       !req.body.cartItems 
+        ){
+            return res.send({message: "Data is inadequate."})
+        }
+        const order = await Order(req.body).save();
+        res.send(order);
+});
+
 //listen to port and launch server
 const port = process.env.PORT || 5000;
 //its a special env variable to set the port number or default 5000 will be set here if it doesnt exist
 app.listen(port, ()=> console.log("server at http://localhost:5000"));
+
+
